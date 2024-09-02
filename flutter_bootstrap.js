8f9b44b2a636ab532558d7e2984e82548e20d169
Udhay Adithya <udhayxd@gmail.com>
2024-09-03 00:06:12 +0530

    feat: about page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3450727284"
+    serviceWorkerVersion: "489401067"
   }
 });
